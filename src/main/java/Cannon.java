593a6e55ae72eb4ddfaaf56cd0239f38058bc770
Marco Roland <69676695+iopoli0@users.noreply.github.com>
2020-09-02 13:26:30 +0200

    PVector til kanon
    Co-Authored-By: Michael30502 <54103011+Michael30502@users.noreply.github.com>

--- src/main/java/Cannon.java
+++ src/main/java/Cannon.java
@@ -1,14 +1,18 @@
 import processing.core.PApplet;
+import processing.core.PVector;
 
 public class Cannon {
 PApplet p;
-
+  PVector CVelocity = new PVector(1,0);
+PVector location = new PVector((p.width/2,p.height));
 Cannon(PApplet p) {
   this.p = p;
+
+
 }
 
   void display(){
-
+p.ellipse(1,1,1,1);
 
   }
 
