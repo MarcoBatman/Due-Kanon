86e33c465476e45e92cd338a064fee1eb9799eb6
MarcoBatman <Marco@terrida.dk>
2020-09-04 11:20:01 +0200

    sprites og kaktus class

---
+++ src/main/java/Cactus.java
@@ -0,0 +1,8 @@
+import processing.core.PApplet;
+import processing.core.PVector;
+
+public class Cactus {
+    PVector location = new PVector(0,0);
+    Cactus(PApplet p){}
+    Cactus(PApplet p,int x){}
+}
