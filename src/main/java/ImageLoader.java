03a25037739c7f07642c1abcb6c266c3aa2cd5c4
MarcoBatman <Marco@terrida.dk>
2020-09-07 10:07:29 +0200

    score added

--- src/main/java/ImageLoader.java
+++ src/main/java/ImageLoader.java
@@ -8,23 +8,23 @@ public class ImageLoader {
         this.p=p;
 
     }
-void load(){
-    barrel = p.loadImage("Barrelmiddle.png");
-    barrel.resize(7*3,17*2*3);
-    tank = p.loadImage("TankTrimmed.png");
-    tank.resize(26*3,25*3);
-    dl = p.loadImage("Duck Duck Go Left Trim.png");
-    dr = p.loadImage("Duck Duck Go Right Trim.png");
-    d = p.loadImage("New Catus.png");
-    d.resize(64*2,64*2);
-    sky =  p.loadImage("skycool.png");
-    sky.resize(1830/2,873/2);
-    bulletdue = p.loadImage("Bullet due.png");
-    bulletdue.resize(61,34);
+    void load(){
+        barrel = p.loadImage("Barrelmiddle.png");
+        barrel.resize(7*3,17*2*3);
+        tank = p.loadImage("TankTrimmed.png");
+        tank.resize(26*3,25*3);
+        dl = p.loadImage("Duck Duck Go Left Trim.png");
+        dr = p.loadImage("Duck Duck Go Right Trim.png");
+        d = p.loadImage("New Catus.png");
+        d.resize(64*2,64*2);
+        sky =  p.loadImage("skycool.png");
+        sky.resize(1830/2,873/2);
+        bulletdue = p.loadImage("Bullet due.png");
+        bulletdue.resize(61,34);
 
 
 
 
-}
+    }
 
 }
